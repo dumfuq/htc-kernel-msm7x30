2cdd62c5fc7843eb0557948160c7ba120309ccfe
jp <jp@gmail.com>
2011-02-02 12:28:21 -0800

    fix merge error for commit to allow ARP to be recieved in low power mode

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Feb 1 22:21:19 PST 2011"
+#define LINUX_COMPILE_TIME "22:21:19"
+#define LINUX_COMPILE_BY "jp"
+#define LINUX_COMPILE_HOST "eddie"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/linux/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.32.28-cyanogenmod-gcc7db01"
